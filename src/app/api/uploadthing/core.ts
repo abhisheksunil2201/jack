b017e31f651bee62ca57cf8cd128318160fa17ec
Abhishek Sunil <abhisheksunil92@gmail.com>
2024-07-01 13:36:52 +0530

    fix uploadthing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,57 +1,34 @@
-import { db } from "@/components/db";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const middleware = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-  if (!user || !user.id) throw new Error("Unauthorized");
-  return { userId: user.id };
-};
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 
-const onUploadComplete = async ({
-  metadata,
-  file,
-}: {
-  metadata: Awaited<ReturnType<typeof middleware>>;
-  file: { key: string; name: string; url: string };
-}) => {
-  const isFileExists = await db.file.findFirst({
-    where: { key: file.key },
-  });
-
-  if (isFileExists) return;
-
-  const createdFile = await db.file.create({
-    data: {
-      key: file.key,
-      name: file.name,
-      userId: metadata.userId,
-      url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-      uploadStatus: "PROCESSING",
-    },
-  });
-  try {
-    await db.file.update({
-      where: { id: createdFile.id },
-      data: { uploadStatus: "SUCCESS" },
-    });
-  } catch (err) {
-    console.log(err);
-
-    await db.file.update({
-      where: { id: createdFile.id },
-      data: { uploadStatus: "FAILED" },
-    });
-  }
-};
-
+// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "16MB" } })
-    .middleware(middleware)
-    .onUploadComplete(onUploadComplete),
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const user = await auth(req);
+
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
